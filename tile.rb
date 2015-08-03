41e38fd1ccfe1f706db2f0ed3395bef2cb090e91
Braelan & Jake <Braelan + Jake @appacademy.io>
2015-08-03 12:08:47 -0400

    First partner switch. Added Board class.

---
+++ tile.rb
@@ -0,0 +1,41 @@
+class Tile
+  attr_accessor :bombed, :flagged, :revealed
+
+
+  def initialize (board, pos)
+    @bombed = false
+    @flagged = false
+    @revealed = false
+    @board = board
+    #position is in [x,y] form
+    @pos = pos
+  end
+
+  def reveal
+    self.revealed = true
+  end
+
+  def bombed?
+    self.bombed
+  end
+
+  def flagged?
+    self.flagged
+  end
+
+  def revealed?
+    self.revealed
+  end
+
+  def neighbors
+    neighbor_array = []
+     board
+  end
+
+  def neighbor_bomb_count
+
+  end
+
+
+
+end
